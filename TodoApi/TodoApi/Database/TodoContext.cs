fd667c16f191e00a52bd3c7ba204e5fe7c6654c9
Mateusz Ostrowski <mateusz.ostro@o2.pl>
2025-06-11 08:26:42 +0200

    added TodoContext

---
+++ TodoApi/TodoApi/Database/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Database
+{
+    public class TodoContext : DbContext
+    {
+
+        public TodoContext(DbContextOptions options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Task> Tasks => Set<Task>();
+
+    }
+}
